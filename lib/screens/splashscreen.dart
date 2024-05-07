8ed13812a23ccb8dc13e097652048e80e25bd5b1
Orihero <orihero@Oriheros-MacBook-Pro.local>
2024-05-07 15:24:11 +0500

    changes

--- lib/screens/splashscreen.dart
+++ lib/screens/splashscreen.dart
@@ -53,7 +53,7 @@ class _SplashScreenState extends State<SplashScreen> {
                           fontSize: 15,
                           fontWeight: FontWeight.w300,
                           fontStyle: FontStyle.normal,
-                          color: Colors.white))),
+                          color: Color(0xffFFFFFF)))),
             ],
           ),
         ),
