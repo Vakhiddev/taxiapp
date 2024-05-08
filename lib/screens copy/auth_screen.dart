c41d1541156aa865fa9a2c7bb0ea75a8a48ea902
Your Name <your_email@example.com>
2024-05-08 14:18:57 +0500

    phone number

--- lib/screens copy/auth_screen.dart
+++ lib/screens copy/auth_screen.dart
@@ -59,7 +59,7 @@ class _AuthScreenState extends State<AuthScreen> {
                     controller: numberController,
                     onChanged: (value) {
                       setState(() {
-                        if (value.length > 12) {
+                        if (value.length > 6) {
                           containerColor = const Color.fromRGBO(255, 214, 0, 1);
                           textColor = const Color.fromRGBO(0, 0, 0, 1);
                           isPush = true;
@@ -102,6 +102,12 @@ class _AuthScreenState extends State<AuthScreen> {
                           ),
                         ),
                       ),
+                      prefixText: '+998 ',
+                      prefixStyle: const TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 25,
+                      ),
                       enabledBorder: OutlineInputBorder(
                         borderSide: const BorderSide(
                           color: Color.fromRGBO(38, 40, 45, 1),
@@ -114,10 +120,76 @@ class _AuthScreenState extends State<AuthScreen> {
                           color: Color.fromRGBO(38, 40, 45, 1),
                         ),
                       ),
-                      contentPadding: EdgeInsets.symmetric(vertical: 10),  // Adjust this value as needed
+                      contentPadding: const EdgeInsets.symmetric(vertical: 10),  // Adjust this value as needed
                     ),
                   ),
                 ),
+
+                // SizedBox(
+                //   height: 60,
+                //   child: TextField(
+                //     controller: numberController,
+                //     onChanged: (value) {
+                //       setState(() {
+                //         if (value.length > 12) {
+                //           containerColor = const Color.fromRGBO(255, 214, 0, 1);
+                //           textColor = const Color.fromRGBO(0, 0, 0, 1);
+                //           isPush = true;
+                //         } else {
+                //           containerColor = const Color.fromRGBO(117, 117, 117, 1); // Gray
+                //           textColor = const Color.fromRGBO(255, 255, 255, 1);
+                //           isPush = false;
+                //         }
+                //       });
+                //     },
+                //     cursorColor: Colors.grey,
+                //     style: GoogleFonts.montserrat(
+                //       textStyle: const TextStyle(
+                //         fontSize: 25,
+                //         fontWeight: FontWeight.w500,
+                //         fontStyle: FontStyle.normal,
+                //         color: Colors.white,
+                //       ),
+                //     ),
+                //     decoration: InputDecoration(
+                //       filled: true,
+                //       fillColor: const Color.fromRGBO(38, 40, 45, 1),
+                //       focusColor: const Color.fromRGBO(38, 40, 45, 1),
+                //       prefixIcon: Padding(
+                //         padding: const EdgeInsets.only(
+                //             right: 24.0,
+                //             left: 16,
+                //             bottom: 10,
+                //             top: 10
+                //         ),
+                //         child: Container(
+                //           height: 36,
+                //           width: 50,
+                //           decoration: BoxDecoration(
+                //             borderRadius: BorderRadius.circular(5),
+                //             image: const DecorationImage(
+                //               image: AssetImage("assets/images/uzbek_flag.png"),
+                //               fit: BoxFit.cover,
+                //             ),
+                //           ),
+                //         ),
+                //       ),
+                //       enabledBorder: OutlineInputBorder(
+                //         borderSide: const BorderSide(
+                //           color: Color.fromRGBO(38, 40, 45, 1),
+                //         ),
+                //         borderRadius: BorderRadius.circular(10),
+                //       ),
+                //       focusedBorder: const OutlineInputBorder(
+                //         borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                //         borderSide: BorderSide(
+                //           color: Color.fromRGBO(38, 40, 45, 1),
+                //         ),
+                //       ),
+                //       contentPadding: EdgeInsets.symmetric(vertical: 10),  // Adjust this value as needed
+                //     ),
+                //   ),
+                // ),
 
 
                 const SizedBox(height: 16),
@@ -139,7 +211,7 @@ class _AuthScreenState extends State<AuthScreen> {
                   child: RichText(
                     textAlign: TextAlign.center,
                     text: TextSpan(
-                      style: TextStyle(
+                      style: const TextStyle(
                         color: Colors.white, // Set the text color to white
                       ),
                       children: [
